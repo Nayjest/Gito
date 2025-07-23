08b5b1495a299cc4d0de4e2000fd596adea58ad4
Nayjest <mail@vitaliy.in>
2025-07-23 14:03:28 +0200

    deb.

--- gito/cli.py
+++ gito/cli.py
@@ -111,6 +111,9 @@ def cmd_review(
     out: str = arg_out()
 ):
     _what, _against = args_to_target(refs, what, against)
+    from pprint import pprint
+    print("v2.0.0: Against")
+    pprint(against)
     asyncio.run(review(
         what=_what,
         against=_against,
