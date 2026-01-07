578eb202bb64c392fc6be5b9656897b245fd36ec
Nayjest <mail@vitaliy.in>
2026-01-07 17:38:35 +0100

    more elegant tests fix for running in forked repos

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,5 @@
 import os
-import pytest
 
-@pytest.fixture(autouse=True)
-def mock_env_vars():
-    """Set dummy environment variables for tests."""
-    # Set a dummy API key to pass microcore configuration validation
-    os.environ["LLM_API_KEY"] = "dummy_key"
-    # Ensure we don't actually try to call any API
-    os.environ["LLM_API_TYPE"] = "openai" 
+import microcore as mc
+
+os.environ["LLM_API_TYPE"] = mc.ApiType.NONE
