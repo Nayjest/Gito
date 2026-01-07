59579fe59c5a4fbe1bfc02460306c2a264554e20
Nayjest <mail@vitaliy.in>
2026-01-07 20:03:11 +0100

    typo fix

--- gito/__main__.py
+++ gito/__main__.py
@@ -1,6 +1,6 @@
 """Allow running the package with `python -m gito`."""
 # package name is required here,
-# otherside windows build made by pyinstaller fails.
+# otherwise windows build made by pyinstaller fails.
 from gito.entrypoint import main
 
 if __name__ == "__main__":
